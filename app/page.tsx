727f0ffe8efb8d3fcc9c3f6c1a5c1b1d73d98837
natasha <nat.parisella@gmail.com>
2023-05-26 13:09:47 +0200

    small fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,40 @@
 "use client";
-
+import dynamic from "next/dynamic";
+import { Suspense, useState } from "react";
+import ArrowLink from "@/components/ArrowLink";
 import Spline from "@splinetool/react-spline";
-import { TiArrowDownOutline, TiArrowRightOutline } from "react-icons/ti";
-import { NavLink } from "./../components/NavLink";
-import { useState } from "react";
+
 import { PacmanLoader } from "react-spinners";
+import { motion } from "framer-motion";
 
 export default function Home() {
-	const [loading, setLoading] = useState(true);
-	const load = () => {
+	const [isLoading, setIsloading] = useState(true);
+	
+	const loaded = () => {
 		setTimeout(() => {
-			setLoading(false);
-		}, 2000);
+			setIsloading(false);
+		}, 500);
 	};
 	return (
 		<div className=" w-full h-full rounded-3xl flex flex-col justify-center items-center">
-			{loading && <PacmanLoader color="#ea38a8" />}
+			{isLoading && (
+				<motion.div
+					initial={{ x: -500, opacity: 0 }}
+					whileInView={{ x: 0, opacity: 1 }}
+					transition={{ type: "spring", duration: 1.2 }}
+					viewport={{ once: true }}
+					className="w-full h-full flex justify-center items-center"
+				>
+					<PacmanLoader size={50} loading color="#ea38a8" />
+				</motion.div>
+			)}
+
 			<Spline
-				onLoad={load}
+				onLoad={loaded}
 				scene="https://prod.spline.design/tX6RieIKm-eOSLJI/scene.splinecode"
-				className="rounded-full z-0 relative"
 			/>
-			<div className="w-full h-10 pb-1 flex justify-end">
-				<NavLink href="/about">
-					<TiArrowDownOutline className="text-4xl text-pink animate-pulse " />
-				</NavLink>
-			</div>
+
+			<ArrowLink nextPath="/about" />
 		</div>
 	);
 }
