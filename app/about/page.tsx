3f2cff190fff53e1baa3818b51bc61a02b347af0
natasha <nat.parisella@gmail.com>
2023-05-27 16:27:15 +0200

    optimization

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,31 +1,5 @@
-"use client";
-import AnimatedText from "@/components/AnimatedText";
-import ArrowLink from "@/components/ArrowLink";
-import { motion } from "framer-motion";
-import { TiArrowDownOutline } from "react-icons/ti";
+import About from "@/components/About";
 
 export default async function AboutPage() {
-	const text = `Hi there, I'm Natasha, a sharp full-stack web developer.
-	 I've honed my skills through tutoring and freelance projects.
-	 Eager for professional growth.
-	 I'm seeking my first full-time role in web development.
-	 Explore my portfolio and let's connect soon!`;
-
-	return (
-		<motion.div
-			initial={{ y: 500, opacity: 0 }}
-			whileInView={{ y: 0, opacity: 1 }}
-			transition={{ type: "spring", duration: 1.2 }}
-			viewport={{ once: true }}
-			className="w-full h-full flex flex-col justify-center items-center rounded-full"
-		>
-			<div className="w-full h-full flex justify-center items-center space-y-4">
-				<AnimatedText
-					text={text}
-					style="text-purple text-2xl space-y-8"
-				/>
-			</div>
-			<ArrowLink nextPath="/projects" />
-		</motion.div>
-	);
+	return <About />;
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,6 +4,7 @@ import { groq } from "next-sanity";
 import WinMockup from "@/components/WinMockup";
 import { motion } from "framer-motion";
 import ArrowLink from "@/components/ArrowLink";
+import Projects from "@/components/Projects";
 
 const query = groq`
   *[_type == "project"] {
@@ -14,17 +15,6 @@ const query = groq`
 
 export default async function ProjectsPage() {
 	const project = await client.fetch(query);
-	console.log(project);
-	return (
-		<motion.div
-			initial={{ y: 500, opacity: 0 }}
-			whileInView={{ y: 0, opacity: 1 }}
-			transition={{ type: "spring", duration: 1.2 }}
-			viewport={{ once: true }}
-			className="w-full h-full bg-black/50 shadow-pink flex flex-col"
-		>
-			<WinMockup projects={project} />
-			<ArrowLink nextPath="/skills" />
-		</motion.div>
-	);
+
+	return <Projects project={project} />;
 }
