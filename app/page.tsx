8c1166f0fb7f099505724d3abb0ab9819e969f69
natasha <nat.parisella@gmail.com>
2023-05-27 16:08:48 +0200

    optimization

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,5 @@
-"use client";
-import dynamic from "next/dynamic";
-import { Suspense, useState } from "react";
-import ArrowLink from "@/components/ArrowLink";
-import Spline from "@splinetool/react-spline";
+import Home from "@/components/Home";
 
-import { PacmanLoader } from "react-spinners";
-import { motion } from "framer-motion";
-
-export default function Home() {
-	const [isLoading, setIsloading] = useState(true);
-	
-	const loaded = () => {
-		setTimeout(() => {
-			setIsloading(false);
-		}, 500);
-	};
-	return (
-		<div className=" w-full h-full rounded-3xl flex flex-col justify-center items-center">
-			{isLoading && (
-				<motion.div
-					initial={{ x: -500, opacity: 0 }}
-					whileInView={{ x: 0, opacity: 1 }}
-					transition={{ type: "spring", duration: 1.2 }}
-					viewport={{ once: true }}
-					className="w-full h-full flex justify-center items-center"
-				>
-					<PacmanLoader size={50} loading color="#ea38a8" />
-				</motion.div>
-			)}
-
-			<Spline
-				onLoad={loaded}
-				scene="https://prod.spline.design/tX6RieIKm-eOSLJI/scene.splinecode"
-			/>
-
-			<ArrowLink nextPath="/about" />
-		</div>
-	);
+export default function HomePage() {
+	return <Home />;
 }
