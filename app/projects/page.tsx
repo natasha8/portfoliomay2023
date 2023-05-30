fdda85b780844fb11b7de2c389000e67c94cebef
natasha <nat.parisella@gmail.com>
2023-05-30 09:40:20 +0200

    fixed import for deploy

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { client } from "../../lib/sanity.client";
 import { groq } from "next-sanity";
-import Projects from "@/components/Projects";
+import Projects from "../../components/Projects";
 
 const query = groq`
   *[_type == "project"] {
