79c17fe41a5cb82c7050fa31d865e63870c32bf2
natasha <nat.parisella@gmail.com>
2023-05-30 10:01:06 +0200

    fixed error in skills page

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,8 +1,5 @@
-"use client";
-import ArrowLink from "@/components/ArrowLink";
-import Skills from "@/components/Skills";
+import SkillsContainer from "@/components/SkillsContainer";
 import { client } from "@/lib/sanity.client";
-import { motion } from "framer-motion";
 import { groq } from "next-sanity";
 
 const query = groq`
@@ -10,18 +7,5 @@ const query = groq`
 `;
 export default async function SkillsPage() {
 	const skills = await client.fetch(query);
-	return (
-		<motion.div
-			initial={{ y: 500, opacity: 0 }}
-			whileInView={{ y: 0, opacity: 1 }}
-			transition={{ type: "spring", duration: 1.2 }}
-			viewport={{ once: true }}
-			className="w-full h-full flex flex-col "
-		>
-			<div className="w-full h-full bg-black/50 flex justify-center items-center">
-				<Skills skills={skills} />
-			</div>
-			<ArrowLink nextPath="/experience" />
-		</motion.div>
-	);
+	return <SkillsContainer skills={skills} />;
 }
---
+++ components/SkillsContainer.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import ArrowLink from "@/components/ArrowLink";
+import Skills from "@/components/Skills";
+import { Skill } from "@/typing";
+import { motion } from "framer-motion";
+
+type Props = {
+	skills: Skill[];
+};
+export default function SkillsContainer({ skills }: Props) {
+	return (
+		<motion.div
+			initial={{ y: 500, opacity: 0 }}
+			whileInView={{ y: 0, opacity: 1 }}
+			transition={{ type: "spring", duration: 1.2 }}
+			viewport={{ once: true }}
+			className="w-full h-full flex flex-col "
+		>
+			<div className="w-full h-full bg-black/50 flex justify-center items-center">
+				<Skills skills={skills} />
+			</div>
+			<ArrowLink nextPath="/experience" />
+		</motion.div>
+	);
+}
